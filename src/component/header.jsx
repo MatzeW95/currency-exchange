40bc3e32570a2479a9fde570b176d084e98fbe6b
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-03-14 21:28:44 +0100

    added mobile menu

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 
 const Header = () => {
+
+    function handleMenuOpen() {
+        document.getElementById("headerNavContainerMobile").style.visibility = "visible";
+    }
+
+    function handleMenuClose() {
+        document.getElementById("headerNavContainerMobile").style.visibility = "hidden";
+    }
+
     return (
-        <div className="header">
-            <a href="#hero" id="headerHeadline">
-                Currency Exchange
-            </a>
-            <div id="headerNavContainer">
-                <a href="#rates" className="navItem">Wechselkurs</a>
-                <a href="#calc" className="navItem">Wechselkursrechner</a>
-                <a href="#history" className="navItem">Historischer Verlauf</a>
+        <div className="nav">
+            <div className="header">
+                <a href="#hero" id="headerHeadline">
+                    Currency Exchange
+                </a>
+                <div id="headerNavContainer">
+                    <a href="#rates" className="navItem">Wechselkurs</a>
+                    <a href="#calc" className="navItem">Wechselkursrechner</a>
+                    <a href="#history" className="navItem">Historischer Verlauf</a>
+                </div>
+                <img src={require("../hamburgerMenu.png")} className="burgerMenu" onClick={handleMenuOpen}></img>
+            </div>
+            <div id="headerNavContainerMobile">
+                <img src={require("../hamburgerMenu.png")} className="burgerMenu" id="burgerMenuClose" onClick={handleMenuClose}></img>
+                <div id="headerContainerMobile">
+                    <a href="#rates" className="navItemMobile" onClick={handleMenuClose}>Wechselkurs</a>
+                    <a href="#calc" className="navItemMobile" onClick={handleMenuClose}>Wechselkursrechner</a>
+                    <a href="#history" className="navItemMobile" onClick={handleMenuClose}>Historischer Verlauf</a> 
+                </div>
             </div>
         </div>
     );
